d57c248a8321a499fee36ee08bee30869ab7e7bc
Bhoomika Valani <bhoomikavalani07@gmail.com>
2021-04-25 18:32:43 +0530

    fixed styling and added timer

---
+++ student/static/js/transition.js
@@ -0,0 +1,18 @@
+var opacity = 0;
+var intervalID = 0;
+window.onload = fadeIn;
+
+function fadeIn() {
+    setInterval(show, 100);
+}
+
+function show() {
+    var card = document.getElementById("card");
+    opacity = Number(window.getComputedStyle(card).getPropertyValue("opacity"));
+    if (opacity < 1) {
+        opacity = opacity + 0.1;
+        card.style.opacity = opacity
+    } else {
+        clearInterval(intervalID);
+    }
+}
