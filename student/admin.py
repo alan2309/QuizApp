7eaf0c207b55898aefc8b9e34e8ba22ba3fac75e
Alankrit Arya <alankritarya15@gmail.com>
2021-04-26 15:26:41 +0530

    updated

--- student/admin.py
+++ student/admin.py
@@ -3,7 +3,7 @@ from student.models import Quiz,Question,Choice,Result
 
 # Register your models here.
 class QuizAdmin(admin.ModelAdmin):
-    list_display=('quiz_name','quiz_end','quiz_subject')
+    list_display=('quiz_subject','quiz_name','quiz_end')
 
 
 admin.site.register(Quiz,QuizAdmin)
