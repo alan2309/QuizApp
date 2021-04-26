13a630e0e6d7a8ced4b1e48595e05002ad548a93
Chaitanya <kchaitanya1911@gmail.com>
2021-04-27 00:22:36 +0530

    Teacherssidedonebackend

--- student/urls.py
+++ student/urls.py
@@ -9,5 +9,11 @@ urlpatterns = [
     path('quiz/<int:quiz_id>/',views.quiz,name='enter-quiz'),
     path('test/<int:quiz_id>/',views.test,name='test-quiz'),
     path('results/<int:quiz_id>/',views.results,name='result-quiz'),
-    path('records/<int:quiz_id>/',views.records,name='student-records')
+    path('records/<int:quiz_id>/',views.records,name='student-records'),
+    path('quiz/',views.QuizListView.as_view(),name='quizlist'),
+    path('quizcreate/',views.QuizCreateView.as_view(),name='quizcreate'),
+    path('quizdetail/<int:pk>/',views.QuizDetailView.as_view(),name='quizdetail'),
+    path('quiz/createquestion/<int:pk>/',views.QuestionCreateView.as_view(),name='questionadd'),
+    path('quiz/delete/<int:pk>/',views.QuizDeleteView.as_view(),name='quizdelete'),
+    path('quiz/create/<int:pk>/question/<int:p>/choice',views.ChoiceCreateView.as_view(),name='choiceadd'),
 ]
